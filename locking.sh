3b8b2eae4a549504be7025855a4cb338616a7869
Operations Research Engineering Software+ <alex@oresoftware.com>
2019-06-05 00:50:21 -0700

    age

--- locking.sh
+++ locking.sh
@@ -11,6 +11,9 @@ fi
 export all_locks="$HOME/.locking/ql/locks";
 mkdir -p "$all_locks";
 
+export count_dir="$HOME/.locking/counts"
+mkdir -p "$count_dir";
+
 
 ql_get_latest(){
   . "$BASH_SOURCE";
@@ -49,6 +52,26 @@ sanitize_lock_name(){
     echo "$1" | sed s'/[— – -]/_/g'
 }
 
+increment_decrement_lock_count(){
+
+ if [[ "$2" != "a" ]]; then
+     echo 0;
+ fi;
+
+(
+flock -x 200
+
+   my_file="$HOME/.locking/counts/counts.json"
+   touch "$my_file"
+   my_str=$(cat "$my_file");
+   typeset -i my_num="${my_str:-"1"}"
+   echo "$((my_num=my_num+$1))" | tee "$my_file"
+
+) 200>>/var/lock/mylockfile
+
+}
+
+
 ql_acquire_lock(){
 
     if [[ -z "$1" ]]; then
@@ -69,6 +92,13 @@ ql_acquire_lock(){
     mkdir "$lock_dir" &> /dev/null && {
         mkfifo "$fifo"
         echo "$$" > "$lock_dir/pid.json"
+        count=`increment_decrement_lock_count 1 "$lock_name"`
+
+        echo "COUNT 1: $count"
+
+        if [[ "$count" > 1 ]]; then
+          echo 'Lock count was greater than 1 after lock acqusition.';
+        fi
         echo 'Acquired lock on first attempt.';
         return 0;
     }
@@ -77,6 +107,13 @@ ql_acquire_lock(){
         echo 'Waiting for lock to be released.' > /dev/stderr;
         local val="$(cat "$fifo")"
         if test "$val" == "unlocked"; then
+          count=`increment_decrement_lock_count 1 "$lock_name"`
+
+        echo "COUNT 2: $count"
+
+        if [[ "$count" > 1 ]]; then
+          echo 'Lock count was greater than 1 after lock acqusition.';
+        fi
           echo 'Acquired lock.';
           break;
         fi
@@ -92,7 +129,8 @@ is_named_pipe_already_opened0(){
 }
 
 is_named_pipe_being_read() {
-   /Users/alex/codes/ores/prompt-command/fcntl "$1"
+#   /Users/alex/codes/ores/prompt-command/fcntl "$1"
+/home/oleg/codes/oresoftware/prompt-command/fcntl "$1"
 }
 
 ql_release_lock(){
@@ -126,12 +164,22 @@ ql_release_lock(){
     # https://unix.stackexchange.com/questions/522877/how-to-cat-named-pipe-without-waiting/522881
 
 
+    count=`increment_decrement_lock_count '-1' "$lock_name"`
+
+
+    if [[ "$count" > 0 ]]; then
+      echo 'Lock count was greater than 0.'
+    fi
+
    if  is_named_pipe_being_read "$fifo"; then
         echo "unlocked" > "$fifo"
    else
        rm -rf "$lock_dir"
        echo "Lock deleted."
    fi
+
+
+
 
 }
 
