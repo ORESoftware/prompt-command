1794950677693e26b35c69882aebdc08fd8897fa
Alexander Mills <alex@alexs-mac.local>
2019-06-07 15:03:20 -0700

    tmp

--- scripts/travis/run-windows.sh
+++ scripts/travis/run-windows.sh
@@ -2,7 +2,6 @@
 
 C:\wget\wget.exe --version
 
+Invoke-WebRequest https://nodejs.org/download/release/latest/node-v12.4.0-win-x64.zip -o 'C:\my_nodejs_download'
 
-unzip https://nodejs.org/download/release/latest/node-v12.4.0-win-x64.zip
-
-mv 'C:\Program Files\nodejs'
+mv 'C:\my_nodejs_download' 'C:\Program Files\nodejs'
