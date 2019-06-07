c39bdee3bf8657dd1852a481c12fee09aedbb922
Alexander Mills <alex@alexs-mac.local>
2019-06-07 11:13:50 -0700

    tmp

--- scripts/travis/run.sh
+++ scripts/travis/run.sh
@@ -4,3 +4,10 @@ echo 'this is the travis build running for nodejs version: '"$1"
 
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[[ -s "$NVM_DIR/nvm.sh" ]] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+
+nvm install "$1"
+nvm use "$1"
